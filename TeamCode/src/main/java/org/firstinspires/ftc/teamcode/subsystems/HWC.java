0c78526be3e8d94f7d78331dea554536825ac04e
RZW-13 <rayneweinstein@gmail.com>
2025-10-14 17:13:05 -0600

    Added servo + motor test file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HWC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HWC.java
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class HWC {
     // Declare empty variables for robot hardware
     public DcMotorEx leftFront, rightFront, leftRear, rightRear;
-    public DcMotorEx topLaunch, leftLaunch, rightLaunch;
+    public DcMotorEx leftLaunch, rightLaunch;
 
     // Other Variables
     Telemetry telemetry;
@@ -39,27 +39,16 @@ public class HWC {
 
         leftLaunch = hardwareMap.get(DcMotorEx.class, "leftLaunch");
         rightLaunch = hardwareMap.get(DcMotorEx.class, "rightLaunch");
-        topLaunch = hardwareMap.get(DcMotorEx.class, "topLaunch");
 
         // Set the direction of motors
         // TODO: UPDATE VALUES WITH NEW BOT
         leftFront.setDirection(DcMotorEx.Direction.REVERSE);
-        rightFront.setDirection(DcMotorEx.Direction.FORWARD);
-        leftRear.setDirection(DcMotorEx.Direction.FORWARD);
-        rightRear.setDirection(DcMotorEx.Direction.FORWARD);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftRear.setDirection(DcMotorEx.Direction.REVERSE);
+        rightRear.setDirection(DcMotorEx.Direction.REVERSE);
 
-        // Set motors to break when power = 0
-        // TODO: REMOVE IF THIS BEHAVIOUR IS NOT DESIRED ON NEW BOT
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        // Run motors using encoder, so that we can move accurately.
-        leftFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        leftRear.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        rightRear.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        leftLaunch.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightLaunch.setDirection(DcMotorSimple.Direction.FORWARD);
     }
     // TODO: ADD ANY HARDWARE RELATED FUNCTIONS BELOW
 }
